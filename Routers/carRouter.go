6cd881cb927fd71cb64a42d44041fbf0544a39b9
tifarin <latifahlatifah443@gmail.com>
2022-10-19 22:12:51 +0700

    first commit

---
+++ Routers/carRouter.go
@@ -0,0 +1,16 @@
+package routers
+
+import (
+	"belajar-gin/controllers"
+	
+	"github.com/gin-gonic/gin"
+)
+
+func StartServer() *gin.Engine {
+	router := gin.Default()
+	router.POST("/cars", controllers.CreateCar)
+	router.PUT("/cars/:carID", controllers.UpdateCar)
+	router.GET("/cars/:carID", controllers.GetCar)
+	router.DELETE("/cars/:carID", controllers.DeleteCar)
+	return router
+}
